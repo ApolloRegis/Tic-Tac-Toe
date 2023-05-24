1a3e7b6f3acb926e230544e971c4ecd13a01a8c4
ApolloRegis <ApolloRegis0@gmail.com>
2023-05-24 05:17:04 -0400

    ADD PLAYER SELECTION FUNCTION.

--- JavaScript/jquery.js
+++ JavaScript/jquery.js
@@ -6,7 +6,19 @@ function getComputerChoice() {
 
 console.log(getComputerChoice());
 
-
+function playRound(playerSelection, computerSelection) { playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === "rock" && computerSelection === "scissors" ||
+    playerSelection === "paper" && computerSelection === "rock" ||
+    playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    } 
+    else if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+    else {
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
 
 
 
