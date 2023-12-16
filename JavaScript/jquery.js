896de3bb830f980e437be14e27feddc560a7eb69
ApolloRegis <ApolloRegis0@gmail.com>
2023-12-16 12:36:47 -0500

    START OVER

--- JavaScript/jquery.js
+++ JavaScript/jquery.js
@@ -1,45 +1,4 @@
 function getComputerChoice() {
-    let choices = ['rock', 'paper', 'scissors'];
-    let randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex]; 
+    
 }
-
-getComputerChoice();
-console.log(getComputerChoice());
-
-function playRound(playerSelection, computerSelection) { 
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection === "rock" && computerSelection === "scissors" ||
-    playerSelection === "paper" && computerSelection === "rock" ||
-    playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! " + playerSelection + " beats " + computerSelection;
-    } 
-    else if (playerSelection === computerSelection) {
-        return "It's a tie!";
-    }
-    else {
-        return "You Lose! " + computerSelection + " beats " + playerSelection;
-    }
-}
-
-
-
-function game() {
-    let round1 = playRound(prompt("Enter Rock, Paper, or Scissors"), getComputerChoice());
-    let round2 = playRound(prompt("Enter Rock, Paper, or Scissors"), getComputerChoice());
-    let round3 = playRound(prompt("Enter Rock, Paper, or Scissors"), getComputerChoice());
-    let round4 = playRound(prompt("Enter Rock, Paper, or Scissors"), getComputerChoice());
-    let round5 = playRound(prompt("Enter Rock, Paper, or Scissors"), getComputerChoice());
-    console.log(round1);
-    console.log(round2);
-    console.log(round3);
-    console.log(round4);
-    console.log(round5);
-}
-
-game();
-
-
-
-
 
