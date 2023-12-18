f3c600f7c31826896094ba977c00fc46fe5f30e0
ApolloRegis <ApolloRegis0@gmail.com>
2023-12-18 08:57:10 -0500

    FUNCTION GETCOMPUTERCHOICE
    Establish a choice for the computer by generating a random number
    and attaching the values "rock", "paper", or "scissors" to the result.

--- JavaScript/jquery.js
+++ JavaScript/jquery.js
@@ -1,4 +1,10 @@
 function getComputerChoice() {
-    
-}
-
+    let choice = Math.random();
+    if(choice<0.33) {
+        return('Rock');
+    } else if(choice<0.66) {
+        return('Paper');
+    } else {
+        return('Scissors');
+    };
+  };
