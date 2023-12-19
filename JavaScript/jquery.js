c2a83fb896be53a026307ea6d6cf439e4ba60fcc
ApolloRegis <ApolloRegis0@gmail.com>
2023-12-19 16:50:32 -0500

    ADD DISPLAY FEATURE
    Add a div and span to display the outputs that are generated when the
    game function is activated by the event listeners on the three selection
    buttons. Set the game function to put its outputs in the display div.

--- JavaScript/jquery.js
+++ JavaScript/jquery.js
@@ -12,34 +12,75 @@ function getComputerChoice() {
   const playRound = function(playerSelection, computerSelection){
 
     if(playerSelection === computerSelection){
-      console.log('It\'s a tie!');
+      return 'It\'s a tie!';
     } else if((playerSelection === 'rock' && computerSelection === 'scissors') || 
     (playerSelection === 'paper' && computerSelection === 'rock') || 
     (playerSelection === 'scissors' && computerSelection === 'paper')) {
-      console.log('You win!' + ' ' + playerSelection + ' ' + 'beats' + ' ' + computerSelection + '.'); 
+      return 'You win!' + ' ' + playerSelection + ' ' + 'beats' + ' ' + computerSelection + '.'; 
     } else {
-      console.log('You lose!' + ' ' + computerSelection + ' ' + 'beats' + ' ' + playerSelection + '.');
+      return 'You lose!' + ' ' + computerSelection + ' ' + 'beats' + ' ' + playerSelection + '.';
+    };
+  };
+
+const body = document.querySelector('body');
+
+const board = document.createElement('div');
+const display = document.createElement('span');
+board.appendChild(display);
+body.appendChild(board);
+
+  let pscore = 0;
+  let cscore = 0;
+
+  const game = function(ply, chlng) {
+    let result = playRound(ply, chlng);
+    
+    if(pscore === 4 && result.includes('You win')){
+      if(pscore > cscore) {
+        pscore += 1;
+      display.textContent = result + ' ' + pscore + ' ' + cscore + ' ' + 'Player wins game!';
+      } else if(pscore < cscore) {
+        pscore += 1;
+      display.textContent = result + ' ' + pscore + ' ' + cscore + ' ' + 'Computer wins game!';
+      } else {
+        pscore += 1;
+      display.textContent = result + ' ' + pscore + ' ' + cscore + ' ' + 'Tie game!';
+      };
+      pscore -= pscore;
+      cscore -= cscore;
+    } else if(result.includes('You win')) {
+      pscore += 1;
+      display.textContent = result + ' ' + pscore + ' ' + cscore;
+    } else if(result.includes('You lose')) {
+      cscore += 1;
+      display.textContent = result + ' ' + pscore + ' ' + cscore;
+    } else if(result.includes('It\'s a tie!')) {
+      display.textContent = result + ' ' + pscore + ' ' + cscore;
     };
   };
   
-  const body = document.querySelector('body');
-  
 const btn1 = document.createElement('button');
 btn1.addEventListener('click', () => {
-    playRound('rock', getComputerChoice());
+    game('rock', getComputerChoice());
 });
 btn1.textContent = 'Rock';
 body.appendChild(btn1);
+
 const btn2 = document.createElement('button');
 btn2.addEventListener('click', () => {
-  playRound('paper', getComputerChoice());
+  game('paper', getComputerChoice());
 });
 btn2.textContent ='Paper';
 body.appendChild(btn2);
+
 const btn3 = document.createElement('button');
 btn3.addEventListener('click', () => {
-  playRound('scissors', getComputerChoice());
+  game('scissors', getComputerChoice());
 });
 btn3.textContent = 'Scissors';
 body.appendChild(btn3);
+
+  
+
+
   
